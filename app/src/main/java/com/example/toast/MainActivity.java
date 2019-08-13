2981a37ccb9223e431157efbd8e4727651f69a0d
Mohamed <mmdabd6@gmail.com>
2019-08-13 09:33:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.toastmessag.ToastMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_error_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastMessage.setError(MainActivity.this, "Error Toast", ToastMessage.LENGTH_SHORT, true).show();
+            }
+        });
+
+        findViewById(R.id.button_i_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastMessage.setInfo(MainActivity.this, "Info Toast", ToastMessage.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_w_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastMessage.setWarning(MainActivity.this, "Warning Toast", ToastMessage.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_s_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastMessage.setNormal(MainActivity.this, "Normal Toast", ToastMessage.LENGTH_LONG).show();
+            }
+        });
+
+        findViewById(R.id.button_n_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastMessage.setSuccess(MainActivity.this,"Success Toast", ToastMessage.LENGTH_LONG, true).show();
+            }
+        });
+    }
+}
---
+++ toastmessage/src/main/java/com/example/toastmessag/Utils.java
@@ -0,0 +1,44 @@
+package com.example.toastmessag;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.content.res.AppCompatResources;
+import android.view.View;
+
+
+public class Utils {
+
+    private Utils() {
+    }
+
+    static Drawable setTintIcon(@NonNull Drawable drawable, @ColorInt int tintColor) {
+        drawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN);
+        return drawable;
+    }
+
+    static Drawable drawableFrame(@NonNull Context context, @ColorInt int tintColor) {
+        return setTintIcon(getDrawable(context, R.drawable.toast_background), tintColor);
+    }
+
+    static void setBackgroundDrawable(@NonNull View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            view.setBackground(drawable);
+        else
+            view.setBackgroundDrawable(drawable);
+    }
+
+    static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) {
+        return AppCompatResources.getDrawable(context, id);
+    }
+
+    static int getColor(@NonNull Context context, @ColorRes int color){
+        return ContextCompat.getColor(context, color);
+    }
+}
